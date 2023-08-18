fa196fbeb53b791d48ecb25f40f31bb3f0c1ac23
Trishala Ghetiya <trishalaghetiya@gmail.com>
2023-08-18 23:45:50 +0530

    added hashing to password

--- controllers/user.js
+++ controllers/user.js
@@ -1,36 +1,48 @@
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
 
-exports.postSignUpUser = (req, res, next) => {
+exports.postSignUpUser = async (req, res, next) => {
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
     const email = req.body.email;
     const password = req.body.password;
-
-    User.create({
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        password: password
-    })
-    .then(result => {
-        res.json({message: 'successfully created new user'});
-        console.log('User added');
-    })
-    .catch(err => {
-        res.status(403).json(err);
-    });
+    try{
+        bcrypt.hash(password, 5, async (err, hash) => {
+            if(err){
+                throw new err;
+            }
+            else{
+                await User.create({
+                    firstName: firstName,
+                    lastName: lastName,
+                    email: email,
+                    password: hash
+                })
+                res.json({message: 'successfully created new user'});
+                console.log('User added');
+            }
+        }) 
+    }
+    catch(err){
+        res.status(403).json({message: 'Something went wrong'});
+    }
 }
 
 exports.postLoginUser = async (req, res, next) => {
     try{
         const user = await User.findOne({ where: { email: req.body.email } });
         if(user!=null){
-            if(user.password === req.body.pass){
-                res.json({message: 'User logged in successfully'});
-            }
-            else{
-                res.status(401).json({ error: "password doesn't match" });
-            }
+            bcrypt.compare(req.body.pass, user.password, async (err, result) => {
+                if(err){
+                    throw new err;
+                }
+                if(result === true){
+                    res.json({message: 'User logged in successfully'});
+                }
+                else{
+                    res.status(401).json({ error: "password doesn't match" });
+                }
+            })
         }
         else{
             res.status(400).json({ error: "User doesn't exist" });
