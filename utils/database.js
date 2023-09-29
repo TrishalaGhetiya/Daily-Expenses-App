97c039e67c0be267ad6c207056c591fe87c58245
Trishala Ghetiya <trishalaghetiya@gmail.com>
2023-09-29 22:52:07 +0530

    deleted unwanted files and folders

--- utils/database.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
-
-const sequelize = new Sequelize(process.env.MYSQL_DEFAULT_DATABASE_SCHEMA, process.env.MYSQL_USERNAME, process.env.MYSQL_PASSWORD, {
-    dialect: 'mysql', 
-    host: 'localhost'
-});
-
-module.exports = sequelize;
